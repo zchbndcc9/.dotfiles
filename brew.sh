6a79e59b52f08b13e293ec7572bdb20738402974
Zach Banducci <zbducci@gmail.com>
2019-05-16 17:26:29 -0500

    Added brew formulae to brew task

---
+++ brew.sh
@@ -0,0 +1,27 @@
+# Install Homebrew
+xcode-select --install
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Update HB and any existing formulae
+brew update
+brew upgrade
+
+
+# Shell and editor 
+brew install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+brew install neovim
+brew install bat
+brew install tree
+brew install boxes
+brew install fortune
+brew install zsh-autosuggestions
+
+# Languages
+brew install node
+brew install yarn
+brew install go
+brew install scala
+brew install sbt
+brew install python
+brew install elixir
