52f2be0f641ce28c99e8ec4705100ca3a774596c
Zach Banducci <zbducci@gmail.com>
2019-05-16 17:37:07 -0500

    Cask for mac apps

---
+++ cask.sh
@@ -0,0 +1,7 @@
+# Apps that I need
+brew cask install iterm2
+brew cask install font-fira-code
+brew cask install firefox
+brew cask install notion
+brew cask install slack
+brew cask install spotify
