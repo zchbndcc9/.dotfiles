a4cb4baa117e415bc508408208759e50dce1bb4e
Zach Banducci <zbducci@gmail.com>
2019-05-17 15:40:01 -0500

    Zsh default term

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ brew bundle
 
 echo "Installing Zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s /bin/zsh
 
 echo "Linking packages..."
 sh ./link.sh
