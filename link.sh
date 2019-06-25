8d9374c38185cf8a50c9f039b8665c1814ed927b
Zach Banducci <zbducci@gmail.com>
2019-06-25 11:34:04 -0500

    mkdir for correct nvim config installation

--- link.sh
+++ link.sh
@@ -1,4 +1,5 @@
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
+mkdir ~/.config/nvim
 ln -sf ~/.dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 ln -sf ~/.dotfiles/Brewfile ~/Brewfile
